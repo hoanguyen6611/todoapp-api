b2c66534e8af0bd154e7830a5da1bb4891c07a24
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-07-06 12:20:11 +0700

    update notication

--- controllers/middlewareController.js
+++ controllers/middlewareController.js
@@ -5,16 +5,15 @@ const middlewareController = {
     const token = req.headers.token;
     if (token) {
       const accessToken = token.split(" ")[1];
-      console.log(accessToken);
       jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
         if (err) {
-          res.status(403).json("Token is not valid");
+          return res.status(403).json("Token is not valid");
         }
         req.user = user;
         next();
       });
     } else {
-      res.status(401).json("You are not authenticated");
+      return res.status(401).json("You are not authenticated");
     }
   },
   //verifyTokenAndAdminAuth
